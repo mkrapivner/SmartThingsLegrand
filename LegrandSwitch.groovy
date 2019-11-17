6ac087063df0d0c6f33d7953f06c9d063966e707
Matt Krapivner <matt@macbook-pro-4.lan>
2019-11-17 15:11:01 -0800

    Correct accounting for state transitions, if we are for some reason stuck in the "Turning On" or "Turning Off" state

--- LegrandSwitch.groovy
+++ LegrandSwitch.groovy
@@ -55,9 +55,9 @@ def propertiesChanged (propList) {
 
     propList.each { key, val ->
         if (key == "Power") {
-            if (val.toBoolean() == true && device.currentState("switch")?.value == "off")
+            if (val.toBoolean() == true && device.currentState("switch")?.value != "on")
                 sendEvent(name: "switch", value: "on")
-            else if (val.toBoolean() == false && device.currentState("switch")?.value == "on")
+            else if (val.toBoolean() == false && device.currentState("switch")?.value != "off")
                 sendEvent(name: "switch", value: "off")
         } else if (key == "PowerLevel") {
             if (val.toInteger() != device.currentState("level")?.value) {
