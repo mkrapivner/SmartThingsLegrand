f4a0276109b61abfc6164ac97584e14e0802162b
Matt Krapivner <matt@testautomator.com>
2026-08-31 20:16:30 -0700

    copyright

--- LegrandConnect.groovy
+++ LegrandConnect.groovy
@@ -1,7 +1,7 @@
 /**
  *  Legrand Connect (Unofficial)
  *
- *  Copyright 2019-2023 Matt Krapivner
+ *  Copyright 2019-2026 Matt Krapivner
  *
  *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  *  in compliance with the License. You may obtain a copy of the License at:
